6271fcc2bb718d133ef2cfa79acac2db0fe8a03b
renster <74253771+rizzle16@users.noreply.github.com>
2024-07-11 13:58:20 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -3,8 +3,9 @@ console.log("JS File was loaded");
 // 35.217665 N, 80,831473 W ->
 
 let map = L.map('map',{
-    center:[ 35.217665, -80,831473]
-})
+    center:[ 35.217665, -80,831473],
+    zoom: 8
+});
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
