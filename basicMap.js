15594dc3a7d5faadc74534e258a7dcc5a8059acb
renster <74253771+rizzle16@users.noreply.github.com>
2024-07-11 14:01:04 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -9,6 +9,5 @@ let map = L.map('map',{
 
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
