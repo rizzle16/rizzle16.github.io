c1d6bd050f83d7db3ab59c77d03bdd8d8e46a045
renster <74253771+rizzle16@users.noreply.github.com>
2024-07-11 14:04:28 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -3,7 +3,7 @@ console.log("JS File was loaded");
 // 35.217665 N, 80,831473 W ->
 
 let map = L.map('map',{
-    center:[ 35.217665, -80,831473],
+    center:[35.217665, -80,831473],
     zoom: 12
 });
 
@@ -12,6 +12,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
- L.marker([ 35.217665, -80,831473]).addTo(map)
+ L.marker([35.217665, -80,831473]).addTo(map)
     .bindPopup('Location:<br> Centeral Piedmount Campus')
     .openPopup();
