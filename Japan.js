a3d76d24370b09065e0c135027efac3a055fb2fa
renster <74253771+rizzle16@users.noreply.github.com>
2024-07-16 13:31:59 -0400

    Update Japan.js

--- Japan.js
+++ Japan.js
@@ -13,7 +13,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
- L.marker(35.6764 , 139.6500],{ 
+ L.marker({[35.6764 , 139.6500],{ 
          
          title: "Japan!"
  }
