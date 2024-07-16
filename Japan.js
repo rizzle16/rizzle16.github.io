37c63ab7cc257403996bb0df3e97ccfb80993990
renster <74253771+rizzle16@users.noreply.github.com>
2024-07-16 14:48:35 -0400

    Update Japan.js

--- Japan.js
+++ Japan.js
@@ -5,7 +5,7 @@ console.log("JS File was loaded");
 
 let map = L.map('map',{
     center:[35.652832, 139.839478],
-    zoom: 10
+    zoom: 12
 });
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
