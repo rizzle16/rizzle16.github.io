4e3f986980bfc510d53bfec9f28b08d57c7ceb37
renster <74253771+rizzle16@users.noreply.github.com>
2024-07-16 13:28:21 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -1,6 +1,7 @@
 console.log("JS File was loaded");
 
-// 35.217665 N, 80,831473 W 
+// 35.217665 N, 80.831473 W 
+// 35.6764 N, 139.6500 - japan
 
 let map = L.map('map',{
     center:[35.2176665, -80.831473],
