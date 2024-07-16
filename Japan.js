384838f3b9a95ad8b67ee18838f6385e19a417e1
renster <74253771+rizzle16@users.noreply.github.com>
2024-07-16 13:44:51 -0400

    Update Japan.js

--- Japan.js
+++ Japan.js
@@ -4,7 +4,7 @@ console.log("JS File was loaded");
 // 35.6764 , 139.6500 - japan
 
 let map = L.map('map',{
-    center:[35.6764, 139.6500 ],
+    center:[35.652832, 139.839478],
     zoom: 15
 });
 
