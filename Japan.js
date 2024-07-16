677d2a50edee237a5e0f531fb8e8c24a842ddfa8
renster <74253771+rizzle16@users.noreply.github.com>
2024-07-16 13:59:04 -0400

    Update Japan.js

--- Japan.js
+++ Japan.js
@@ -9,7 +9,9 @@ let map = L.map('map',{
 });
 
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            {
+                maxZoom: 19,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
